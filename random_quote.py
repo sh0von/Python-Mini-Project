dc91753090924005ab9be1c7aab816bef6631371
shovon <minarshovon909@gmail.com>
2022-09-08 14:46:30 +0000

    Random Quote

--- random_quote.py
+++ random_quote.py
@@ -6,9 +6,12 @@ headers = {
 	"X-RapidAPI-Key": "4d8389b856msh70ace6f646a7918p101b48jsn0dce6744806f",
 	"X-RapidAPI-Host": "quotes15.p.rapidapi.com"
 }
-
+##custom header response
 response = requests.request("GET", url, headers=headers)
+##load for json
 x=response.json()
+##finding path to json
 quote=(x['content'])
 author=(x['originator']['name'])
+##print
 print(author,"said that ", quote)
