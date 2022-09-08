14bce20ad808ee51b3257d0c3ed9806d300a429c
shovon <minarshovon909@gmail.com>
2022-09-08 14:47:42 +0000

    Random Quote

--- random_quote.py
+++ random_quote.py
@@ -1,7 +1,7 @@
 import requests
 
+##api details by rapid api
 url = "https://quotes15.p.rapidapi.com/quotes/random/"
-
 headers = {
 	"X-RapidAPI-Key": "4d8389b856msh70ace6f646a7918p101b48jsn0dce6744806f",
 	"X-RapidAPI-Host": "quotes15.p.rapidapi.com"
